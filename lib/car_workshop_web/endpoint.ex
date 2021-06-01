d00df5d91e76cac199aef73cd382e9a5ee9a5f05
Santiago Cardona <santiagocardo80@gmail.com>
2021-06-01 10:10:39 -0500

    Deploy: Heroku

--- lib/car_workshop_web/endpoint.ex
+++ lib/car_workshop_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule CarWorkshopWeb.Endpoint do
   ]
 
   socket "/socket", CarWorkshopWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
